068a81a4748719a3aaf8ff55bca19feb836e5d17
Jesse White <jwhite08@gmail.com>
2013-07-20 15:19:27 -0400

    Setup the etc and share folders if missing.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from __future__ import with_statement
 from fabric.api import env, run, cd, sudo, parallel
 from fabric.exceptions import CommandTimeout
+from fabric.contrib import files
 from utils import if_host_offline_ignore
 from settings import DEV_USER, OPENNMS_SRC, BUILD_HOST, CLUSTER_HOSTS, OPENNMS_HOME, SHARED_HOME
 import os
@@ -10,7 +11,7 @@ env.user = 'root'
 
 # Default to the build host for particular tasks
 tasks_to_exec = set(env.tasks)
-tasks_for_build_host = set(['build', 'rebuild', 'predeploy'])
+tasks_for_build_host = set(['build', 'rebuild', 'predeploy', 'install'])
 if tasks_for_build_host.intersection(tasks_to_exec):
     env.hosts = [BUILD_HOST]
 
@@ -100,6 +101,13 @@ def predeploy():
         run("ln -sf %s/share share" % SHARED_HOME)
         run("chown -R root:root *")
 
+        if not files.exists("%s/etc" % SHARED_HOME):
+            run("cp -R etc.pristine %s/etc" % SHARED_HOME)
+
+        if not files.exists("%s/share" % SHARED_HOME):
+            run("cp -R share.pristine %s/share" % SHARED_HOME)
+
+def install():
     with cd(OPENNMS_BIN):
         run("./runjava -s")
         run("./install -dis")
